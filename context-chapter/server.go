6b010a38ef1ef18decaefe5c8d62089de8e8d0d3
Terry Drozdowski <terry@xymox.dev>
2022-07-20 17:05:52 -0700

    checkpoint

---
+++ context-chapter/server.go
@@ -0,0 +1,22 @@
+package context_chapter
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return
+		}
+		fmt.Fprint(w, data)
+	}
+}
